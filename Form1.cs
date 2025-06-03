7ea178c4040874ed38808f0754178368b47f1eda
Ogurchikk <clashofbodaring@gmail.com>
2025-06-03 15:46:20 +0500

    Shop

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shop_play_Leshukov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Shop pyaterochka = new Shop();
+        List<string> productText = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            pyaterochka.CreateProduct("Кола", 85, 200);
+            pyaterochka.CreateProduct("Сок \"Добрый\"", 100, 10); List<string> podtit = pyaterochka.WriteAllProducts();
+            
+            foreach (var prod in podtit)
+            {
+                listBox1.Items.Add(prod);
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = "";
+            
+            pyaterochka.Sell("Сок \"Добрый\"", ref text);
+            productText = pyaterochka.WriteAllProducts();
+            listBox1.Items.Clear();
+            foreach(var product in productText)
+            {
+                listBox1.Items.Add(product);
+            }
+            if (text != string.Empty)
+                MessageBox.Show(text);
+        }
+    }
+}
---
+++ Shop.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_play_Leshukov
+{
+    class Shop
+    {
+        private Dictionary<Product, int> products;
+
+        public Shop()
+        {
+            products = new Dictionary<Product, int>();
+        }
+
+        public void AddProduct(Product product, int count)
+        {
+            products.Add(product,count);
+        }
+
+        public List<string> WriteAllProducts()
+        {
+            List<string> podtit = new List<string>();
+            
+            string title = "";
+            foreach (var product in products)
+            {
+                podtit.Add(product.Key.GetInfo() + "Количество " + product.Value);
+            }
+            return podtit;
+        }
+
+        public void CreateProduct(string name, decimal price, int count)
+        {
+            products.Add(new Product(name, price), count);
+        }
+
+        public void Sell(Product product, ref string text)
+        {
+            if (products.ContainsKey(product))
+            {
+                if (products[product] == 0)
+                {
+                    text = "Нет в наличии";
+                }
+                else
+                {
+                    products[product]--;
+                }
+            }
+            else
+            {
+                text = "Товар не найден";
+            }
+        }
+        public void Sell(string ProductName, ref string text)
+        {
+            string t = "";
+            Product ToSell = FindByName(ProductName);
+            if (ToSell != null)
+            {
+                this.Sell(ToSell,ref t);
+                text = t;
+            }
+            else
+            {
+                text = "Товар не найден";
+            }
+        }
+        public Product FindByName(string name)
+        {
+            foreach (var product in products.Keys)
+            {
+                if (product.Name == name)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+    }
+}
