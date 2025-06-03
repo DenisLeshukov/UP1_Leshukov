5b45a68c6a53daefa323cc5a7f3cfb5ab6e6a8b9
DenisLeshukov <Leshukov80@list.ru>
2025-06-04 00:36:54 +0500

    Доделанная работа

---
+++ Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace Shop_play_Leshukov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Корзина = new System.Windows.Forms.ListBox();
+            this.Каталог_товаров = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.практика1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.показатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.скрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.практика2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Корзина);
+            this.panel1.Controls.Add(this.Каталог_товаров);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.numericUpDown3);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Location = new System.Drawing.Point(4, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(785, 410);
+            this.panel1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label6.Location = new System.Drawing.Point(7, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 22);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Корзина:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(133, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(175, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Кол-во которое продаст магазин";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(133, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Название товара";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(536, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Стоимость товара";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(541, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Название товара";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(557, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Кол-во товара";
+            // 
+            // Корзина
+            // 
+            this.Корзина.FormattingEnabled = true;
+            this.Корзина.Location = new System.Drawing.Point(11, 165);
+            this.Корзина.Name = "Корзина";
+            this.Корзина.Size = new System.Drawing.Size(198, 173);
+            this.Корзина.TabIndex = 11;
+            // 
+            // Каталог_товаров
+            // 
+            this.Каталог_товаров.FormattingEnabled = true;
+            this.Каталог_товаров.Location = new System.Drawing.Point(233, 165);
+            this.Каталог_товаров.Name = "Каталог_товаров";
+            this.Каталог_товаров.Size = new System.Drawing.Size(549, 173);
+            this.Каталог_товаров.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(11, 354);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(198, 51);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Продать товары";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Продать_товары);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(642, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(600, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(162, 33);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Добавить товар";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Добавить_товар);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(233, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(549, 51);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Показать элементы повторно";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Показать_повторно);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(301, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Добавить в корзину";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Добвить_в_корзину);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(642, 64);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(642, 38);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(7, 43);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.практика1ToolStripMenuItem,
+            this.практика2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // практика1ToolStripMenuItem
+            // 
+            this.практика1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.показатьToolStripMenuItem,
+            this.скрытьToolStripMenuItem});
+            this.практика1ToolStripMenuItem.Name = "практика1ToolStripMenuItem";
+            this.практика1ToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.практика1ToolStripMenuItem.Text = "Практика 1";
+            this.практика1ToolStripMenuItem.Click += new System.EventHandler(this.практика1ToolStripMenuItem_Click);
+            // 
+            // показатьToolStripMenuItem
+            // 
+            this.показатьToolStripMenuItem.Name = "показатьToolStripMenuItem";
+            this.показатьToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.показатьToolStripMenuItem.Text = "Показать";
+            this.показатьToolStripMenuItem.Click += new System.EventHandler(this.Показать_практику1);
+            // 
+            // скрытьToolStripMenuItem
+            // 
+            this.скрытьToolStripMenuItem.Name = "скрытьToolStripMenuItem";
+            this.скрытьToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.скрытьToolStripMenuItem.Text = "Скрыть";
+            this.скрытьToolStripMenuItem.Click += new System.EventHandler(this.Скрыть_практику1);
+            // 
+            // практика2ToolStripMenuItem
+            // 
+            this.практика2ToolStripMenuItem.Name = "практика2ToolStripMenuItem";
+            this.практика2ToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.практика2ToolStripMenuItem.Text = "Практика 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox Корзина;
+        private System.Windows.Forms.ListBox Каталог_товаров;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem практика1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem практика2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem показатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem скрытьToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace Shop_play_Leshukov
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, int> Basket = new Dictionary<string, int>();
+        List<string> names = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Shop pyaterochka = new Shop();
+
+
+        
+
+        
+
+        private void Показать_повторно(object sender, EventArgs e)//Показать все товары в магазине +прибыль магазина элементы
+        {
+            showEl();
+            Каталог_товаров.Items.Add($"Прибыль магазина {pyaterochka.GetProf()}");
+        }
+
+        private void Добвить_в_корзину(object sender, EventArgs e)//Продажа товара
+        {
+
+            if (textBox1.Text != "")//Проверка на пустое имя товара
+            {
+                string name = textBox1.Text;
+                int count = Convert.ToInt32(numericUpDown1.Value);
+                if (Basket.ContainsKey(name)) // проверка на то существует ли данный товар в корзине
+                {
+                    MessageBox.Show("Товар уже добавлен");
+                }
+                else
+                {
+                    if (pyaterochka.CheckforCount(name, count)) // проверка на количество идобавление в словарь
+                    {
+                        Корзина.Items.Add($"{name} {count}");
+                        names.Add(name);
+                        Basket.Add(name, count);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите имя товара");
+            }
+        }
+
+        private void showEl()//Показать все товары в магазине
+        {
+            Каталог_товаров.Items.Clear();
+            string q = pyaterochka.WriteAllProducts();
+            string[] s = q.Split('\n');
+            for (int i = 0; i < s.Length; i++)
+            {
+                Каталог_товаров.Items.Add(s[i]);
+            }
+
+            Каталог_товаров.Items.Add($"Прибыль магазина {pyaterochka.GetProf()}");//Вывод прибыли магазина
+        }
+
+        private void Добавить_товар(object sender, EventArgs e)//Добавить товар
+        {
+            if (textBox2.Text != "")
+            {
+                string name = textBox2.Text;
+                int count = Convert.ToInt32(numericUpDown2.Value);
+                int price = Convert.ToInt32(numericUpDown3.Value);
+                if (pyaterochka.FindByName(name) == null)//Проверка на наличие товара
+                {
+                    pyaterochka.CreateProduct(name, price, count);//Создание продукта
+                    pyaterochka.ProfitCount(price, count, 10);//Подсчёт трат на этот продукт
+                    showEl();
+
+                }
+                else
+                {
+                    MessageBox.Show("Такой товар уже есть");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите имя товара");
+            }
+        }
+
+        private void Продать_товары(object sender, EventArgs e) // Кнопка продажи товаров
+        {
+            for (int i = 0; i < names.Count; i++) // проходимся по количеству предметов в названиях 
+            {
+                pyaterochka.Sell(names[i], Basket[names[i]]);
+            }
+            Корзина.Items.Clear();
+            showEl();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void практика1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Показать_практику1(object sender, EventArgs e) // показать практику 1
+        {
+            panel1.Visible = true;
+        }
+
+        private void Скрыть_практику1(object sender, EventArgs e) // скрыть практику 1
+        {
+            panel1.Visible = false;
+        }
+    }
+}
